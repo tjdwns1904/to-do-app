bb6ea8e971f0668b072e7b51229d44cc10634540
tjdwns99 <beans2853@gmail.com>
2025-03-02 20:34:28 +0900

    [TD-10] feat: Add footer links * To personal sites

--- frontend/src/components/Common/Footer.tsx
+++ frontend/src/components/Common/Footer.tsx
@@ -1,4 +1,10 @@
 import IMAGES from "@/assets/images/images";
+import { Link } from "react-router-dom";
+
+const INSTAGRAM = "https://www.instagram.com/tjdwns99/";
+const FACEBOOK = "https://www.facebook.com/profile.php?id=100008141106316";
+const GITHUB = "https://github.com/tjdwns1904";
+const LINKEDIN = "https://www.linkedin.com/in/seongjoon-hong-193a742b9/";
 
 function Footer() {
     return (
@@ -17,10 +23,10 @@ function Footer() {
                 </div>
             </div>
             <div className="contact-container">
-                <a href="/" className="yt"></a>
-                <a href="/" className="ig"></a>
-                <a href="/" className="fb"></a>
-                <a href="/" className="twit"></a>
+                <Link to={LINKEDIN} target="_blank" className="linkedin"/>
+                <Link to={INSTAGRAM} target="_blank" className="ig"/>
+                <Link to={FACEBOOK} target="_blank" className="fb"/>
+                <Link to={GITHUB} target="_blank" className="github"/>
             </div>
         </div>
     )
