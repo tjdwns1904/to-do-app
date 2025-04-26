90b9a0757634430b9cfd8352a0afcfc1c5556005
tjdwns99 <beans2853@gmail.com>
2025-04-26 23:48:22 +0900

    [TD-29] style: Organize code using prettier

--- frontend/src/pages/Home/_components/HomeSection2Skeleton.tsx
+++ frontend/src/pages/Home/_components/HomeSection2Skeleton.tsx
@@ -1,35 +1,35 @@
-import React from 'react';
-import Skeleton from 'react-loading-skeleton';
-import 'react-loading-skeleton/dist/skeleton.css';
+import React from "react";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 const HomeSection2Skeleton: React.FC = () => {
-    return (
-        <div className='row'>
-            <div className="col-sm-6">
-                <div className="col-sm-8 mx-auto mt-[250px] text-start lg:mt-[300px]">
-                    <Skeleton height={20} width={150} className="mb-[30px]" />
-                    <Skeleton height={30} width={300} className="mb-[30px]" />
-                    <Skeleton height={20} width={400} />
-                </div>
-                <div className="col-sm-8 mx-auto mt-[250px] text-start lg:mt-[300px]">
-                    <Skeleton height={20} width={150} className="mb-[30px]" />
-                    <Skeleton height={30} width={300} className="mb-[30px]" />
-                    <Skeleton height={20} width={400} />
-                </div>
-                <div className="col-sm-8 mx-auto mt-[250px] mb-[100px] text-start lg:mt-[300px]">
-                    <Skeleton height={20} width={150} className="mb-[30px]" />
-                    <Skeleton height={30} width={300} className="mb-[30px]" />
-                    <Skeleton height={20} width={400} />
-                </div>
-            </div>
-            <div className="col-sm-6">
-                <Skeleton
-                    className="col-sm-10 sticky z-0 mt-[180px] aspect-6/4 max-h-[550px] rounded-[20px]"
-                    height={300}
-                />
-            </div>
+  return (
+    <div className="row">
+      <div className="col-sm-6">
+        <div className="col-sm-8 mx-auto mt-[250px] text-start lg:mt-[300px]">
+          <Skeleton height={20} width={150} className="mb-[30px]" />
+          <Skeleton height={30} width={300} className="mb-[30px]" />
+          <Skeleton height={20} width={400} />
         </div>
-    );
+        <div className="col-sm-8 mx-auto mt-[250px] text-start lg:mt-[300px]">
+          <Skeleton height={20} width={150} className="mb-[30px]" />
+          <Skeleton height={30} width={300} className="mb-[30px]" />
+          <Skeleton height={20} width={400} />
+        </div>
+        <div className="col-sm-8 mx-auto mt-[250px] mb-[100px] text-start lg:mt-[300px]">
+          <Skeleton height={20} width={150} className="mb-[30px]" />
+          <Skeleton height={30} width={300} className="mb-[30px]" />
+          <Skeleton height={20} width={400} />
+        </div>
+      </div>
+      <div className="col-sm-6">
+        <Skeleton
+          className="col-sm-10 sticky z-0 mt-[180px] aspect-6/4 max-h-[550px] rounded-[20px]"
+          height={300}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default HomeSection2Skeleton;
--- frontend/src/pages/Home/_components/HomeSection3Skeleton.tsx
+++ frontend/src/pages/Home/_components/HomeSection3Skeleton.tsx
@@ -1,27 +1,35 @@
-import React from 'react';
-import Skeleton from 'react-loading-skeleton';
-import 'react-loading-skeleton/dist/skeleton.css';
+import React from "react";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 const HomeSection3Skeleton: React.FC = () => {
-    return (
-        <div>
-            <Skeleton height={80} width={'40%'} className="mb-5 mx-auto" />
-            <Skeleton width={80} height={30} className="text-muted mb-[16px] mx-auto w-fit rounded-[10px] bg-[#e3e3e3] px-[10px] py-[5px]"/>
-            <div className='row items-center justify-between'>
-                <div className="col-sm-4">
-                    <Skeleton height={200} width={300} className="rounded-[20px]" />
-                </div>
-                <div className='col-sm-4'>
-                    <Skeleton height={300} width={10} className="rounded-[20px]" />
-                </div>
-                <div className="col-sm-4">
-                    <Skeleton height={20} width={250} className="mb-2" />
-                    <Skeleton height={20} width={200} />
-                </div>
-            </div>
-            <Skeleton width={150} height={30} className="mx-auto mt-[16px] mb-0 w-fit rounded-[10px] bg-[#e3e3e3] px-[10px] py-[5px]"/>
+  return (
+    <div>
+      <Skeleton height={80} width={"40%"} className="mx-auto mb-5" />
+      <Skeleton
+        width={80}
+        height={30}
+        className="text-muted mx-auto mb-[16px] w-fit rounded-[10px] bg-[#e3e3e3] px-[10px] py-[5px]"
+      />
+      <div className="row items-center justify-between">
+        <div className="col-sm-4">
+          <Skeleton height={200} width={300} className="rounded-[20px]" />
         </div>
-    );
+        <div className="col-sm-4">
+          <Skeleton height={300} width={10} className="rounded-[20px]" />
+        </div>
+        <div className="col-sm-4">
+          <Skeleton height={20} width={250} className="mb-2" />
+          <Skeleton height={20} width={200} />
+        </div>
+      </div>
+      <Skeleton
+        width={150}
+        height={30}
+        className="mx-auto mt-[16px] mb-0 w-fit rounded-[10px] bg-[#e3e3e3] px-[10px] py-[5px]"
+      />
+    </div>
+  );
 };
 
 export default HomeSection3Skeleton;
